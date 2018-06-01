abe819b3534caafd3fe3e7c6b5858b7f33ed4ceb
tmfadhlul <tmfadhlul@gmail.com>
2018-06-01 23:17:48 +0700

    udpate before redux

---
+++ src/reducers/store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux'
+
+const reducer = (state, action) => {
+    return 'unknown'
+}
+
+const store = createStore(reducer, 0)
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Image, ScrollView } from 'react-native';
-import { Button } from 'native-base';
+import { Button, Content } from 'native-base';
 import { Swiper } from 'react-native-swiper';
 
 import { Color } from '../config/style'
@@ -13,18 +13,18 @@ export default class Home extends Component {
     render() {
         return (
             <View style={{ backgroundColor: Color.darkDongker, flex: 1 }}  >
-                <View style={{ flex: 1.3, backgroundColor: Color.medDongker }} >
-                    <Slider />
-                    <SliderCurrency />
-                    <SliderAnnounce />
-                </View>
-                <View style={{ flex: 0.2, backgroundColor: Color.medDongker, marginVertical: '2%' }} >
-                    <ActionHome />
-                </View>
-                <View style={{ flex: 0.7 }} >
-                    <View>
+                    <View style={{ flex: 1.3, backgroundColor: Color.medDongker }} >
+                        <Slider />
+                        <SliderCurrency />
+                        <SliderAnnounce />
                     </View>
-                </View>
+                    <View style={{ flex: 0.2, backgroundColor: Color.medDongker, marginVertical: '2%' }} >
+                        <ActionHome />
+                    </View>
+                    <View style={{ flex: 0.7 }} >
+                        <View>
+                        </View>
+                    </View>
             </View>
         )
     }
